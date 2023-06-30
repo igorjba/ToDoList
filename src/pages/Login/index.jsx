5a9a76fc5f6192b911d31ddaad737a100ce45cd1
Igor <igorjbahia@gmai.com>
2023-06-30 15:54:39 -0300

    :sparkles: feat: Rotas configuradas.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+import { Navigate, useNavigate } from "react-router-dom"
+
 export function Login() {
+    const navigate = useNavigate()
+
+    function verifyToken() {
+        const token = 'blablabla'
+        if (token) navigate('/todos')
+    }
+
+    useEffect(() => {
+        verifyToken()
+    }, [])
+
     return (
         <h1>Login</h1>
     )
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,30 @@
-import { Route, Routes } from 'react-router-dom'
+import { Outlet, Route, Routes, Navigate } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Todos } from './pages/Todos'
 import { About } from './pages/About'
 import { Error } from './pages/Error'
+import { Header } from './components/Header'
+
+export function ProtectedRoutes({ redirectTo }) {
+    const token = 'blablabla'
+    return token ? <Outlet /> : <Navigate to={redirectTo} />
+}
 
 export function MainRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/todos" element={<Todos />} />
-            <Route path="/about" element={<About />} />
+            <Route element={
+                <>
+                    <Header />
+                    <ProtectedRoutes redirectTo="/" />
+                </>
+            }>
+
+                <Route path="/todos" element={<Todos />} />
+                <Route path="/about" element={<About />} />
+            </Route>
+
             <Route path="/*" element={<Error />} />
         </Routes>
     )
