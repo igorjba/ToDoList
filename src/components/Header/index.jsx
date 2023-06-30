c7f80bd086b4003ba0dcfe51865874a62f49c57f
Igor <igorjbahia@gmai.com>
2023-06-30 16:20:27 -0300

    :sparkles: feat: Iniciada a criação do header.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,24 @@
+import './styles.css';
+import logoImage from '../../assets/logo.svg'
+import { NavLink } from 'react-router-dom';
+
 export function Header() {
+
+    function logout() {
+        localStorage.removeItem('token')
+    }
+
     return (
-        <h1>Header</h1>
+        <header className='header'>
+            <div className="container header-container">
+                <img src={logoImage} alt="Logo da Cubos Academy" />
+                <nav className='navigation'>
+                    <span>Igor | </span>
+                    <NavLink to='/todos' className="navigation__item" >Tarefas</NavLink>
+                    <NavLink to='/about' className='navigation__item' >Sobre</NavLink>
+                    <NavLink onClick={logout} to='/' className='navigation__item' >Sair</NavLink>
+                </nav>
+            </div>
+        </header>
     )
 }
